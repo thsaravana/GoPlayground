705a55d0379f1abc51717849f0cefcfd8e218819
Henry <th.saravana@gmail.com>
2019-07-22 15:00:10 +0530

    Simple variables

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import ("fmt")
 
 func main() {
-	fmt.Printf("What is wrong")
+	fmt.Printf("What is wrong?")
 }
---
+++ variables/assignment.go
@@ -0,0 +1,13 @@
+package main
+
+import ("fmt")
+
+func main() {
+
+	var a = 4
+	var b = 9
+
+	var sum = a + b
+
+	fmt.Printf("Sum is %d", sum)
+}
