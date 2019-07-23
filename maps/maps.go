36e475308f8d0201c3b356965bf3bab7be0688d0
Henry <th.saravana@gmail.com>
2019-07-23 11:04:50 +0530

    Working with maps

--- maps/maps.go
+++ maps/maps.go
@@ -11,4 +11,25 @@ func main() {
 
 	map2 := map[int] string { 1 : "One", 2 : "Two" }
 	fmt.Println(map2)
+
+	// Working with Maps
+	fmt.Println("Working with maps")
+	map3 := map[int] string {}
+	map3[1] = "One"
+	map3[2] = "Two"
+	map3[3] = "Three"
+	map3[1] = "Uno"
+	fmt.Println(map3)
+
+	value, exists := map3[1]
+	if exists {
+		fmt.Printf("1 is %s\n", value)
+	}
+
+	value1, exists1 := map3[11]
+	if exists1 {
+		fmt.Printf("1 is %s\n", value1)
+	} else {
+		fmt.Println("Nothing to see here")
+	}
 }
